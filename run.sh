59ac48b785e0486063eef19867a10bf8032df8be
Adrian Ibañez <hello@adrianibanez.info>
2025-01-30 18:23:52 +0100

    changes

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 
 
 #ERLANG_COOKIE=testlitest AUTH_USERNAME=admin AUTH_PASSWORD=nimda_1234 mix phx.server
-SMTP2GO_APIKEY=api-8C229504AB2F45248218104C8732F36D ERLANG_COOKIE=testlitest AUTH_USERNAME=admin AUTH_PASSWORD=nimda_1234 iex --name "node@localhost" --cookie testlitest -S mix phx.server
+MIX_ENV=dev SMTP2GO_APIKEY=api-8C229504AB2F45248218104C8732F36D ERLANG_COOKIE=testlitest AUTH_USERNAME=admin AUTH_PASSWORD=nimda_1234 iex --name "node-sensocto@localhost" --cookie testlitest -S mix phx.server
 #TOKEN_SIGNING_SECRET=aaa SECRET_KEY_BASE=aölsjdföalksjdflaskjdfölasjdöflkjasödlfkjasökldjfalösdjföaslkdjföasldfjöasdfkjlasöljfd ERLANG_COOKIE=testlitest AUTH_USERNAME=admin AUTH_PASSWORD=nimda_1234 MIX_ENV=prod DATABASE_URL=ecto://postgres:postgres@localhost/sensocto_dev iex --name "node@localhost" --cookie testlitest -S mix phx.server

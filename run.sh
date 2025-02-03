f9a5867f478fade7baa86ac665cbf68490235261
Adrian Ibañez <hello@adrianibanez.info>
2025-02-03 17:35:08 +0100

    change envs

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #sudo sysctl -w kern.maxfiles=100000
 #sudo sysctl -w kern.maxfilesperproc=100000
-
-
+source .env
 #ERLANG_COOKIE=testlitest AUTH_USERNAME=admin AUTH_PASSWORD=nimda_1234 mix phx.server
-PLUG_EDITOR="vscode://file/__FILE__:__LINE__" MIX_ENV=dev SMTP2GO_APIKEY=api-8C229504AB2F45248218104C8732F36D ERLANG_COOKIE=testlitest AUTH_USERNAME=admin AUTH_PASSWORD=nimda_1234 iex --name "node-sensocto@localhost" --cookie testlitest -S mix phx.server
+iex --name "node-sensocto@Adrians-MacBook-Pro.local" --cookie testlitest -S mix phx.server
 #TOKEN_SIGNING_SECRET=aaa SECRET_KEY_BASE=aölsjdföalksjdflaskjdfölasjdöflkjasödlfkjasökldjfalösdjföaslkdjföasldfjöasdfkjlasöljfd ERLANG_COOKIE=testlitest AUTH_USERNAME=admin AUTH_PASSWORD=nimda_1234 MIX_ENV=prod DATABASE_URL=ecto://postgres:postgres@localhost/sensocto_dev iex --name "node@localhost" --cookie testlitest -S mix phx.server

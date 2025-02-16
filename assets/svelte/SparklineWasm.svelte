aa22535c5a96d76e6855e1310c39041babe42e00
Adrian Ibañez <hello@adrianibanez.info>
2025-02-16 17:53:11 +0100

    timewindow debug

--- assets/svelte/SparklineWasm.svelte
+++ assets/svelte/SparklineWasm.svelte
@@ -351,7 +351,7 @@
 {#if true}
     <div class="text-xs">
         <!--Data points {data.length},  width: {width} width: {width} height: {height} cntOffsetWidth: {cntElement?.offsetWidth}-->
-        maxsamples: {maxsamples}, timewindow: {timewindow}
+        data: {data.length}, max: {maxsamples}, timewindow: {timewindow}
         <!--<pre>{JSON.stringify(data, null, 2)}</pre>-->
     </div>
 {/if}
